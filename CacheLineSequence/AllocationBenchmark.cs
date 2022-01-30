3e572a05e46dad86e566c666ab73652f7c3d1627
vitols <vitolsov@gmail.com>
2022-01-30 09:52:12 +0200

    Add allocation benchmark

---
+++ CacheLineSequence/AllocationBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using CacheLineSequence;
+
+namespace CacheLineSequence
+{
+    [ShortRunJob]
+    [NativeMemoryProfiler]
+    [MemoryDiagnoser]
+    public class AllocationBenchmark
+    {
+        public IEnumerable<long[]> Data()
+        {
+            return new long[][]
+            {
+                Enumerable.Range(0, 64).Select(l => (long)l).ToArray(),
+                Enumerable.Range(0, 1024).Select(l => (long)l).ToArray(),
+                Enumerable.Range(0, 4096).Select(l => (long)l).ToArray(),
+                Enumerable.Range(0, 4096*4).Select(l => (long)l).ToArray()
+            };
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Data))]
+        public void Allocation(long[] array)
+        {
+            using var clSeq = new CacheLineSequence<long>(array);
+        }
+    }
+}
